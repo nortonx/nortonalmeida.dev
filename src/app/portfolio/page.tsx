8b61fceebdff7dfd0ca6c5eddea07452e34ff3d6
Norton Almeida <norton.almeida@gmail.com>
2025-04-30 17:17:42 -0300

    feat - Portfolio page: Fixed HTML URL

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,12 +4,13 @@ import Link from "next/link"
 interface Repo {
   id: number;
   name: string;
-  url: string;
+  html_url: string;
   description: string;
 }
 
 export default async function Portfolio() {
   const repos = await getRepos()
+  console.log(repos)
   return (
     <div>
       <h1 className="text-2xl font-bold">This is the Portfolio Page</h1>
@@ -18,7 +19,7 @@ export default async function Portfolio() {
         return <li key={repo?.id} className="my-4">
           <div>
             <span className="font-bold">{repo?.name}</span>:
-            <Link href={repo?.url} className="text-slate-400" data-testid={repo.name}> {repo?.url}</Link>
+            <Link href={repo?.html_url} className="text-slate-400" data-testid={repo.name} target={"_blank"}> {repo?.description}</Link>
             <div className="italic">Description - {repo?.description}</div>
           </div>
         </li>
