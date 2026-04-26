56c99cde16905e40d79735a7564da90b32aa19b1
Norton Almeida <norton.almeida@gmail.com>
2026-04-26 19:44:28 -0300

    fix: update nnav-links

--- src/components/nav-menubar.tsx
+++ src/components/nav-menubar.tsx
@@ -8,28 +8,41 @@ import {
 } from "@/components/ui/navigation-menu"
 
 import packageJson from "../../package.json"
+import Link from "next/link"
 
 export default function NavMenubar() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuLink href="/">Home</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/">Home</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuLink href="#experience">Experience</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/#experience">Experience</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuLink href="/portfolio">Portfolio</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/portfolio">Portfolio</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuLink href="#education">Education</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/#education">Education</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuLink href="#skills">Skills</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/#skills">Skills</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuLink href="#about">About</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/#about">About</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <span className="text-xs text-muted-foreground ml-2 px-3 py-2 cursor-default">
